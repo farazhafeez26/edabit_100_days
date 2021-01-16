bb9bd44041fb25aaf93eeeb50a97a1bccdf4d587
farazhafeez26 <faraz.hafiz@gmail.com>
2021-01-16 13:50:08 +0100

    practice objects and keys

--- arrays.js
+++ arrays.js
@@ -12,3 +12,30 @@ function ascDesNone(arr, str) {
 
   if (str == "None") return arr;
 }
+
+function toArray(obj) {
+  const all_keys = Object.keys(obj);
+
+  return all_keys.map((x) => {
+    return [x, obj[x]];
+  });
+}
+
+//x is the key and obj[x] is the value
+
+function giveMeSomething(a) {
+  return "something" + " " + a;
+}
+
+function triArea(base, height) {
+  return (base * height) / 2;
+}
+
+function getVoteCount(votes) {
+  return votes.upvotes - votes.downvotes;
+}
+
+//another variation
+function getVoteCount(votes) {
+  return votes["upvotes"] - votes["downvotes"];
+}
